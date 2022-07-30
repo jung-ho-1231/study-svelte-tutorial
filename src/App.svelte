378b271d1d883ca11b7b6d9ce47e05e49a5bd735
jung-ho-1231 <parkjung-1231@naver.com>
2022-07-30 18:01:05 +0900

    6. Bindings - Select multiple

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,53 @@
 <script>
-    let questions = [
-        {id: 1, text: 'where did you go to school?'},
-        {id: 2, text: 'What is mothers name'},
-        {id: 3, text: 'What is another personal fact that an attacker could easily find with Google?'}
+    let scoops = 1;
+    let flavours = ['Mint choc chip'];
+
+    let menu = [
+        "Cookies and cream",
+        "Mint choc chip",
+        "Raspberry ripple"
     ];
 
-    let selected;
-
-    let answer = '';
-
-    function handleSubmit() {
-        alert(`answered question ${selected.id} (${selected.text} with "${answer})`)
+    function join(flavors) {
+        if (flavours.length === 1) return flavours[0];
+        return `${flavours.slice(0, -1).join(', ')} and ${flavours[flavours.length - 1]}`;
     }
-    
+
 </script>
 
-<h2>Insecurity questions</h2>
+<h2>Size</h2>
+<label>
+    <input type=radio bind:group={scoops} value={1}>
+    One scoop
+</label>
 
-<form on:submit|preventDefault={handleSubmit}>
+<label>
+    <input type=radio bind:group={scoops} value={2}>
+    Two scoops
+</label>
 
-    <select bind:value={selected} on:change={ () => answer = ''}>
-        {#each questions as question}
-            <option value={question}>
-                {question.text}
-            </option>
-        {/each}
-    </select>
+<label>
+    <input type=radio bind:group={scoops} value={3}>
+    Three scoops
+</label>
 
-    <input bind:value={answer}>
+<h2>Flavours</h2>
 
-    <button type="submit" disabled={!answer}>
-        Submit
-    </button>
+<select multiple bind:value={flavours}>
+    {#each menu as flavour}
+        <option value={flavour}>
+            {flavour}
+        </option>
+    {/each}
+</select>
 
-</form>
-
-
-<p>selected question {selected ? selected.id : '[waiting...]'}</p>
-
-<style>
-    input {
-        display: block;
-        width: 500px;
-        max-width: 100%;
-    }
-</style>
+{#if flavours.length === 0}
+    <p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+    <p>Can't order more flavours than scoops!</p>
+{:else}
+    <p>
+        You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
+        of {join(flavours)}
+    </p>
+{/if}
