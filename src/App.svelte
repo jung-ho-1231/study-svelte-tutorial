26865f4646e0cc24ea1c61b983ebbe57553ee8e0
jung-ho-1231 <parkjung-1231@naver.com>
2022-07-30 17:08:39 +0900

    6. Bindings - Group inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,54 @@
 <script>
-    let yes = false;
+    let scoops = 1;
+    let flavours = ['Mint choc chip'];
+
+    let menu = [
+        'Cookies and cream',
+        'Mint choc chip',
+        'Raspberry ripple'
+    ]
+
+    function join(flavors) {
+        if (flavors.length === 1) return flavors[0]
+        return `${flavors.slice(0, -1).join(', ')} and ${flavors[flavors.length - 1]}`;
+    }
 </script>
 
+<h2>Size</h2>
+
+
+<label>
+    <input type="radio" bind:group={scoops} name="scopes" value={1}>
+    One scope
+</label>
+
 <lable>
-    <input type="checkbox" bind:checked={yes}>
-    Yes! Send me regular email spam
+    <input type="radio" bind:group={scoops} name="scopes" value={2}>
+    Two scopes
 </lable>
 
+<lable>
+    <input type="radio" bind:group={scoops} name="scopes" value={3}>
+    Three scopes
+</lable>
 
-{#if yes}
-    <p>Thank you. We will bombard your inbox and sell your personal details</p>
+<h2>Flavours</h2>
+
+{#each menu as flavour}
+    <lable>
+        <input type="checkbox" bind:group={flavours} name="flavors" value={flavour}>
+        {flavour}
+    </lable>
+{/each}
+
+
+{#if flavours.length === 0}
+    <p>Please select at least one flavour</p>
+{:else if flavours.length > scoops}
+    <p>Can't order more flavours then scoops!</p>
 {:else}
-    <p>You must opt in continue. if you're not paying, you're the product</p>
+    <p>
+        You ordered {scoops} {scoops === 1 ? 'scoop' : 'scoops'}
+        of {join(flavours)}
+    </p>
 {/if}
