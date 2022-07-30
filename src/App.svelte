2fa44fbd1a37c9b76fff58272930570959194903
jung-ho-1231 <parkjung-1231@naver.com>
2022-07-30 13:50:28 +0900

    3. Props - Spread props

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script>
-    import Nested from "./Nested.svelte";
+    import Info from "./info.svelte";
+
+    const pkg = {
+        name : 'svelte',
+        version : 3,
+        speed : 'blazing',
+        website: 'https://svelte.dev'
+    };
+
 </script>
 
-<Nested answer={42} />
-<Nested />
+<Info {...pkg} />
+
