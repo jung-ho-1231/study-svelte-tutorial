f312505147cdad17e342870abdc4b0141fe9a3a8
jung-ho-1231 <parkjung-1231@naver.com>
2022-07-30 16:20:53 +0900

    6. Bindings - Checkbox inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,15 @@
 <script>
-    let a = 1;
-    let b = 2;
+    let yes = false;
 </script>
 
-<label>
-    <input type="number" bind:value={a} min=0 max=10>
-    <input type="range" bind:value={a} min=0 max=10>
-</label>
+<lable>
+    <input type="checkbox" bind:checked={yes}>
+    Yes! Send me regular email spam
+</lable>
 
 
-<label>
-    <input type="number" bind:value={b} min=0 max=10>
-    <input type="range" bind:value={b} min=0 max=10>
-</label>
-
-<p>{a} + {b} = {a + b}</p>
-
-<style>
-    label {
-        display: flex
-    }
-
-    input, p {
-        margin: 6px
-    }
-</style>
+{#if yes}
+    <p>Thank you. We will bombard your inbox and sell your personal details</p>
+{:else}
+    <p>You must opt in continue. if you're not paying, you're the product</p>
+{/if}
